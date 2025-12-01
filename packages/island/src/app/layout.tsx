3721cad8f7f5171cf52b60a1b90c3c7c83664fc9
rikashi-shifu <harryliow229@gmail.com>
2025-12-01 22:04:36 +0800

    feat: successfully separated all packages

--- packages/island/src/app/layout.tsx
+++ packages/island/src/app/layout.tsx
@@ -13,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased m-0 p-0">
-        {children}
-      </body>
+      <body className="antialiased m-0 p-0">{children}</body>
     </html>
   );
 }
